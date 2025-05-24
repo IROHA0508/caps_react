09094dda6eaf5efa3a8e7351cb8f9ad692107c4a
iRoHa0508 <siwonl0508@naver.com>
2025-05-24 17:11:48 +0900

    온보드 폰트 조절

--- caps/src/Onboarding.js
+++ caps/src/Onboarding.js
@@ -18,14 +18,15 @@ function Onboarding({ onComplete }) {
 
   return (
     <div className="Onboarding">
-      {/* <h2>온보딩 페이지</h2> */}
       <div className ="onboard-image-wrapper"> 
         <img src={onboard_picture} alt="온보딩 이미지" className="onboard-image" />
       </div>
-      <p>
+
+      <p style={{ fontFamily: 'Pretendard Variable, sans-serif' }}>
         실패도 응원해주는 인공지능 코치<br />
         <strong>LIA</strong>와 함께 루틴을 만들어보세요
       </p>
+
 
       <div className="google-login-wrapper">
         <GoogleLogin
