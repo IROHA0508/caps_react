2457b90154af5f8df9676566521ceb1fbd9c21c8
iRoHa0508 <happyday7862@gmail.com>
2025-05-26 03:27:49 +0900

    텍스트 LIA div 분리 및 CSS 수정

--- caps/src/pages/MainPage/MainPage.js
+++ caps/src/pages/MainPage/MainPage.js
@@ -31,22 +31,25 @@ function MainPage() {
 
   return (
     <GoogleOAuthProvider clientId="829026060536-f7dpc16930esthgnn97soleggvmv3o16.apps.googleusercontent.com">
-      <div className="main-page-container">
-
+     <div className="main-page-container">
         {user && <Header user={user} onLogout={handleLogout} />}
 
-        <div className="lia-background-area">
-          <div className="lia-text">
-            안녕하세요<br />
-            저는 LIA예요<br />
-            당신을 위해 디지털 세계에서 왔어요.<br />
-            말 걸어주시면 언제든 함께할게요!
+        <div className="lia-wrapper">
+          <div className="lia-text-box">
+            <p>
+              안녕하세요<br />
+              저는 LIA예요<br />
+              당신을 위해 디지털 세계에서 왔어요.<br />
+              말 걸어주시면 언제든 함께할게요!
+            </p>
           </div>
 
-          <div className="lia-overlay">
-            <button className="button" onClick={() => setShowTalkOptions(true)}>
-              LIA와 이야기하기
-            </button>
+          <div className="lia-character-area">
+            <div className="lia-button-wrapper">
+              <button className="button" onClick={() => setShowTalkOptions(true)}>
+                LIA와 이야기하기
+              </button>
+            </div>
           </div>
         </div>
 
@@ -55,6 +58,7 @@ function MainPage() {
           onClose={() => setShowTalkOptions(false)}
         />
       </div>
+
     </GoogleOAuthProvider>
   );
 }
