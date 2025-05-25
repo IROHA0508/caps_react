996c30cedfbc85bbd03c2ae21dc7ab9465175c48
iRoHa0508 <siwonl0508@naver.com>
2025-05-25 20:25:39 +0900

    스플래시 페이지 전환 초 전환

--- caps/src/pages/Splash/Splash.js
+++ caps/src/pages/Splash/Splash.js
@@ -9,7 +9,7 @@ function Splash() {
   useEffect(() => {
     const timer = setTimeout(() => {
       navigate('/onboarding'); // 2초 후 온보딩 페이지로 이동
-    }, 2000);
+    }, 1000);
 
     return () => clearTimeout(timer); // 컴포넌트 언마운트 시 타이머 정리
   }, [navigate]);
