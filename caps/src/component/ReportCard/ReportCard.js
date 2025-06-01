531496745c17b6d07ad4a879fa88440b12a5c617
iRoHa0508 <siwonl0508@naver.com>
2025-06-01 20:19:01 +0900

    ReportCard 컴포넌트 추가 및 코드 수정

---
+++ caps/src/component/ReportCard/ReportCard.js
@@ -0,0 +1,32 @@
+// src/component/ReportCard/ReportCard.js
+import React from 'react';
+import './ReportCard.css';
+
+function ReportCard({ date, activities, feedback }) {
+  return (
+    <div className="report-card">
+      <h3 className="card-date">{date}</h3>
+
+      {activities.map((activity, index) => (
+        <div className="card-section" key={index}>
+          <div className="activity-header">
+            <strong>{activity.title}</strong>
+            <span>{activity.time}</span>
+          </div>
+          <p>{activity.comment}</p>
+          <span className="activity-icon">{activity.icon}</span>
+        </div>
+      ))}
+
+      {feedback && (
+        <div className="card-section">
+          <strong>오늘 상태는?</strong>
+          <p>{feedback}</p>
+          <span className="activity-icon">🧠</span>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default ReportCard;
--- caps/src/pages/ReportPage/ReportPage.js
+++ caps/src/pages/ReportPage/ReportPage.js
@@ -1,6 +1,8 @@
 import React, { useState, useRef, useEffect } from 'react';
 import Header from '../../component/Header/Header';
 import testgif from '../../pictures/iroha.gif';
+import ReportCard from '../../component/ReportCard/ReportCard';
+
 import './ReporePage.css';
 
 function ReportPage() {
@@ -20,6 +22,31 @@ function ReportPage() {
     return () => slider.removeEventListener('scroll', handleScroll);
   }, []);
 
+
+  const dummyData = [
+    {
+      date: '2025.05.25 (일)',
+      activities: [
+        { title: '산책', time: '5분', comment: '조금씩이라도 밖에 나가보는 건 기분 전환에 좋아!', icon: '✔️' },
+        { title: '토익 공부', time: '1시간 30분', comment: '평소보다 긴 시간 공부하려고 노력했어!', icon: '✔️' },
+      ],
+      feedback:
+        '오전엔 잠시 기분이 안좋았지만 산책 후에 많이 나아졌어. 오늘은 오랫동안 집중하려고 노력했어! 처음엔 잘 안될 수 있지만, 시작한 것만으로도 이미 많이 성장한거야.',
+    },
+    {
+      date: '2025.05.26 (월)',
+      activities: [
+        { title: '운동', time: '20분', comment: '꾸준한 루틴이 쌓이고 있어!', icon: '💪' },
+      ],
+      feedback: '월요일이지만 잘 해냈어. 흐름을 계속 이어가자.',
+    },
+    {
+      date: '2025.05.27 (화)',
+      activities: [],
+      feedback: '피곤했지만 일정을 지키려고 한 점이 멋졌어!',
+    },
+  ];
+
   return (
     <div>
       <Header title="통계 리포트" />
@@ -27,21 +54,23 @@ function ReportPage() {
       <div className="report-page">
         <div className="report-header">
           <div className="quote-box">
-            <div className="quote-img-wrapper">
-              <img src={testgif} alt="테스트 gif" className="character-image" />
-            </div>
+
             <div className="quote-text-wrapper">
               <p className="quote">
                 한번 실패하는게, 아무것도 안 하는 것보다 성장하는 거야<br />충분히 잘했어!
               </p>
             </div>
+
+            <div className="quote-img-wrapper">
+              <img src={testgif} alt="테스트 gif" className="character-image" />
+            </div>
           </div>
         </div>
-
+        
         <div className="report-slider" ref={sliderRef}>
-          <div className="card-slide">카드 1</div>
-          <div className="card-slide">카드 2</div>
-          <div className="card-slide">카드 3</div>
+          {dummyData.map((data, i) => (
+            <ReportCard key={i} {...data} />
+          ))}
         </div>
 
         <div className="dot-indicator">
