015d9353788cb4c85159fe88b3040f029fd9e07e
iRoHa0508 <siwonl0508@naver.com>
2025-06-01 20:15:29 +0900

    리포트 페이지 수정

--- caps/src/pages/ReportPage/ReportPage.js
+++ caps/src/pages/ReportPage/ReportPage.js
@@ -1,35 +1,60 @@
-// ReportPage.js
-import React from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import Header from '../../component/Header/Header';
 import testgif from '../../pictures/iroha.gif';
+import './ReporePage.css';
 
 function ReportPage() {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const sliderRef = useRef(null);
+
+  const handleScroll = () => {
+    const scrollLeft = sliderRef.current.scrollLeft;
+    const width = sliderRef.current.offsetWidth;
+    const index = Math.round(scrollLeft / width);
+    setCurrentIndex(index);
+  };
+
+  useEffect(() => {
+    const slider = sliderRef.current;
+    slider.addEventListener('scroll', handleScroll);
+    return () => slider.removeEventListener('scroll', handleScroll);
+  }, []);
+
   return (
     <div>
       <Header title="통계 리포트" />
 
       <div className="report-page">
-        {/* 상단 타이틀 및 캐릭터 영역 */}
         <div className="report-header">
-          <button className="back-button">←</button>
-          <h2>통계 리포트</h2>
           <div className="quote-box">
-            <img src={testgif} alt="테스트 gif" className="character-image" />
-            <p className="quote">한번 실패하는게, 아무것도 안 하는 것보다 성장하는 거야<br />충분히 잘했어!</p>
+            <div className="quote-img-wrapper">
+              <img src={testgif} alt="테스트 gif" className="character-image" />
+            </div>
+            <div className="quote-text-wrapper">
+              <p className="quote">
+                한번 실패하는게, 아무것도 안 하는 것보다 성장하는 거야<br />충분히 잘했어!
+              </p>
+            </div>
           </div>
         </div>
 
-        {/* 슬라이드 카드 영역 */}
-        <div className="report-slider">
+        <div className="report-slider" ref={sliderRef}>
           <div className="card-slide">카드 1</div>
           <div className="card-slide">카드 2</div>
           <div className="card-slide">카드 3</div>
         </div>
-      </div>
 
+        <div className="dot-indicator">
+          {[0, 1, 2].map((i) => (
+            <span
+              key={i}
+              className={`dot ${currentIndex === i ? 'active' : ''}`}
+            />
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
 
 export default ReportPage;
-
