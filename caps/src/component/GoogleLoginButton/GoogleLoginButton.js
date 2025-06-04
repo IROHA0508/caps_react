75abe407039c6d1cfca9b4f6143dd27935f3ff40
iRoHa0508 <happyday7862@gmail.com>
2025-06-04 23:05:07 +0900

    서버 주석 해제

--- caps/src/component/GoogleLoginButton/GoogleLoginButton.js
+++ caps/src/component/GoogleLoginButton/GoogleLoginButton.js
@@ -20,30 +20,30 @@ const GoogleLoginButton = ({ onLoginSuccess }) => {
 
 
       // ✅ 프로젝트 서버에 JWT 토큰 요청 -> 서버 키면 주석 해제
-      // const res = await fetch(`https://${serverIP}/users/google`, {
-      //   method: 'POST',
-      //   headers: {
-      //     'Content-Type': 'application/json',
-      //   },
-      //   body: JSON.stringify({
-      //     credential: credentialResponse.credential,
-      //   }),
-      // });
-      // console.log('🔗 서버 주소:', `https://${serverIP}/users/google`);
-      // console.log('🔗 프로젝트 서버 응답:', res);
-      // // console.log('🔗 서버 응답 상태:', res.status);
-      // const result = await res.json();
-      // const token = result?.data?.token;
-      // if (token) {
-      //   localStorage.setItem('server_jwt_token', token);
-      //   console.log('✅프로젝트 서버용 JWT 토큰 저장 완료:', token);
-      // } else {
-      //   console.warn('⚠️프로젝트 서버 응답에 토큰이 없습니다.');
-      // }
+      const res = await fetch(`https://${serverIP}/users/google`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          credential: credentialResponse.credential,
+        }),
+      });
+      console.log('🔗 서버 주소:', `https://${serverIP}/users/google`);
+      console.log('🔗 프로젝트 서버 응답:', res);
+      // console.log('🔗 서버 응답 상태:', res.status);
+      const result = await res.json();
+      const token = result?.data?.token;
+      if (token) {
+        localStorage.setItem('server_jwt_token', token);
+        console.log('✅프로젝트 서버용 JWT 토큰 저장 완료:', token);
+      } else {
+        console.warn('⚠️프로젝트 서버 응답에 토큰이 없습니다.');
+      }
 
     // ✅ 서버 JWT 토큰 요청이 성공했을 경우에만 메인 페이지로 이동
-  // if (res.ok && onLoginSuccess){
-    if (onLoginSuccess) {
+    if (res.ok && onLoginSuccess){
+    // if (onLoginSuccess) {
         console.log("✅ onLoginSuccess 콜백 호출됨");
         onLoginSuccess(decoded);
       }
